401f1543adbbd04078039afaf31a1b46b409ab8e
Steven Tan <steven@steventan.com.au>
2022-01-29 16:19:55 +1100

    add adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
+/* https://github.com/sveltejs/kit/tree/master/packages/adapter-static
+	- use adapter-static to generate static site.
+*/
+// import adapter from '@sveltejs/adapter-static';
+
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
