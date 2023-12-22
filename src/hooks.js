00cf0a030112bc938468846b5ef55e6cb1e7916d
Steven <steven@steventan.com.au>
2023-12-23 08:33:34 +1100

    update to sveltekit 2

--- src/hooks.js
+++
@@ -1,8 +0,0 @@
-export const getSession = (req) => {
-  return {
-    apiRoot: import.meta.env.VITE_API_ROOT,
-    user: {
-
-    }
-  }
-}
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
-/* https://github.com/sveltejs/kit/tree/master/packages/adapter-static
-	- use adapter-static to generate static site.
-*/
-// import adapter from '@sveltejs/adapter-static';
-
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({ postcss: true })
-	],
-
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
